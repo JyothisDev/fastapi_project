c586563439a5c521101325160982ffd7609c6e91
JyothisDev <jyothis.hiworth@gmail.com>
2025-08-04 15:02:48 +0530

    new

--- app/routes/messages.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.schemas import MessageIn, MessageOut
-from app.crud import save_message
-
-router = APIRouter()
-
-@router.post("/messages", response_model=MessageOut)
-async def post_message(msg: MessageIn):
-    return await save_message(msg.content)
